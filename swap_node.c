77e1d737277bd33bebd20da5f78e12eff0e794ad
arunjethi <148993111+arunjethi@users.noreply.github.com>
2023-10-25 23:28:59 +0530

    Add files via upload

---
+++ swap_node.c
@@ -0,0 +1,122 @@
+// Swaping the node: even with even node also odd with odd node.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
+
+void swap(Node *first, Node *second)
+{
+    Node *temp = first->next;
+    temp->next = first;
+    first->next = second->next;
+    second->next = temp;
+}
+
+Node *Swap_node(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+
+    Node *itr = head;
+    if (itr->next != NULL && itr->next->next != NULL)
+    {
+        head = itr->next->next;
+    }
+    else
+        return head;
+
+    while (itr)
+    {
+        Node *slow = itr, *fast = itr->next;
+
+        if (slow != NULL)
+        {
+            if (fast != NULL)
+            {
+                Node *ptr_s = NULL, *ptr_f = NULL;
+                if (slow->next->next != NULL)
+                {
+                    ptr_s = slow->next->next;
+                    swap(slow, slow->next->next);
+                }
+                else
+                    break;
+
+                if (fast->next->next != NULL)
+                {
+                    ptr_f = fast->next->next;
+                    swap(fast, fast->next->next);
+                    ptr_s->next = ptr_f;
+                }
+                else
+                    break;
+            }
+        }
+        itr = itr->next->next;
+        if (fast->next)
+        {
+            if (fast->next->next)
+            {
+                if (fast->next->next->next)
+                {
+                    fast->next = itr->next->next;
+                }
+            }
+        }
+    }
+    return head;
+}
+Node *insertion_at_begin(Node *head)
+{
+    Node *temp = (Node *)malloc(sizeof(Node));
+    if (!temp)
+    {
+        printf("Memory not allocated!\n");
+        return NULL;
+    }
+
+    int val;
+    printf("Enter data: ");
+    scanf("%d", &val);
+    temp->data = val;
+    temp->next = head;
+
+    head = temp;
+
+    return head;
+}
+void display(Node *head)
+{
+    if (head == NULL)
+        return;
+    printf("\nNode data:      ");
+    Node *itr = head;
+    while (itr)
+    {
+        printf("%d \t", itr->data);
+        itr = itr->next;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    int choice;
+// 1 12 1 11 1 10 1 9 1 8 1 7 1 6 1 5 1 4 1 3 1 2 1 1 1 -1 1 -2 1 -3 1 -4 1 -5 1 -6 1 -7 1 -8 1 -9 1 -10 1 -11 1 -12 0
+    while (1)
+    {
+        printf("Enter 1 to continue: ");
+        scanf("%d", &choice);
+        if (choice == 1)
+            head = insertion_at_begin(head);
+        else
+            break;
+    }
+    head = Swap_node(head);
+    display(head);
+    return 0;
+}
